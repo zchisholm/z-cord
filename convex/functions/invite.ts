732b899b11caae039d7c54ba06c90936644beb00
zchisholm <shakwanc21@gmail.com>
2024-11-09 22:15:43 -0500

    Created invites API

---
+++ convex/functions/invite.ts
@@ -0,0 +1,66 @@
+import { v } from "convex/values";
+import { assertServerMember, authenticatedMutation } from "./helpers";
+import { Id } from "../_generated/dataModel";
+import { query, QueryCtx } from "../_generated/server";
+
+
+const getInvite = async (ctx: QueryCtx, inviteId: Id<"invites">) => {
+    const invite = await ctx.db.get(inviteId);
+    if (!invite) {
+        throw new Error("Invite not found.")
+    } else if (invite.expiresAt && Date.now() > invite.expiresAt) {
+        throw new Error("Invite expired");
+    } else if (invite.maxUses && invite.uses >= invite.maxUses) {
+        throw new Error("Invite has been used too many times");
+    }
+    return invite;
+};
+
+export const get = query({
+  args: { id: v.id("invites") },
+  handler: async (ctx, { id }) => {
+    const invite = await getInvite(ctx, id);
+    const server = await ctx.db.get(invite.serverId);
+    if (!server) {
+      throw new Error("Server not found");
+    }
+
+    return {
+      ...invite,
+      server,
+    };
+  },
+});
+
+
+export const create = authenticatedMutation({
+  args: {
+    serverId: v.id("servers"),
+    expiresAt: v.optional(v.number()),
+    maxUses: v.optional(v.number()),
+  },
+  handler: async (ctx, { serverId, expiresAt, maxUses }) => {
+    await assertServerMember(ctx, serverId);
+    const inviteId = await ctx.db.insert("invites", {
+      serverId,
+      expiresAt,
+      maxUses,
+      uses: 0,
+    });
+    return inviteId;
+  },
+});
+
+export const join = authenticatedMutation({
+  args: { id: v.id("invites") },
+  handler: async (ctx, { id }) => {
+    const invite = await getInvite(ctx, id);
+    await ctx.db.insert("serverMembers", {
+      serverId: invite.serverId,
+      userId: ctx.user._id,
+    });
+      await ctx.db.patch(id, {
+          uses: invite.uses + 1
+      })
+  },
+});
